1e785a20f500e8f7508e2046a12e8e08fb8e37ec
hanyt <hanyt@pingpongx.com>
2018-11-23 17:40:05 +0800

    drama static init

---
+++ drama-static/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dramaStaticApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dramaStaticApp
+ */
+angular.module('dramaStaticApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ drama-static/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dramaStaticApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dramaStaticApp
+ */
+angular.module('dramaStaticApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

ef708b4fb79ee5ed748d1529797f69bd6cb80a9e
hanyt <hanyt@pingpongx.com>
2019-01-21 16:01:59 +0800

    完成eureka调整

--- drama/src/main/java/com/cute/play/drama/DramaApplication.java
+++ drama/src/main/java/com/cute/play/drama/DramaApplication.java
@@ -3,14 +3,12 @@ package com.cute.play.drama;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hanyuting
  * @since 2018/11/16
  */
 @EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
 public class DramaApplication {
 
